aebfcde1f46530d04d056475dc427832741bff6a
Henry Chung <chungthehao@gmail.com>
2020-03-07 23:29:40 +0700

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,13 +9,15 @@ const App = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const incrementCount1 = () => setCount1(count1 + 1);
-  const incrementCount2 = () => setCount2(count2 + 1);
+  const incrementCount1 = useCallback(() => setCount1(count1 + 1), [count1]);
+  const incrementCount2 = useCallback(() => setCount2(count2 + 1), [count2]);
 
   // This func does NOT need to be re-generated (a new version), it doesn't have any dependencies
-  const logName = () => console.log('Henry Chung'); // Generating this func new every single time on every single render
+  const logName = useCallback(() => console.log('Henry Chung'), []); // Generating this func new every single time on every single render
 
-  functions.add(logName);
+  // functions.add(logName);
+  functions.add(incrementCount1);
+  functions.add(incrementCount2);
 
   console.log(functions);
 
